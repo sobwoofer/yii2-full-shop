6e4eba2a98be1ae17ecedade0b21cabcf2756bf1
volynets <volynets@papirus.com.ua>
2017-08-08 10:12:02 +0300

    rename table user to users (migration)

--- shop/entities/User.php
+++ shop/entities/User.php
@@ -85,7 +85,7 @@ class User extends ActiveRecord implements IdentityInterface
      */
     public static function tableName()
     {
-        return '{{%user}}';
+        return '{{%users}}';
     }
 
     /**

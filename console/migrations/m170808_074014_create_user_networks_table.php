efd175feed8367d7d8fb9d21aed6fe90383b1830
volynets <volynets@papirus.com.ua>
2017-08-08 11:36:29 +0300

    create user networks table migration

---
+++ console/migrations/m170808_074014_create_user_networks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `user_networks`.
+ */
+class m170808_074014_create_user_networks_table extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
+        $this->createTable('{{%user_networks}}', [
+            'id' => $this->primaryKey(),
+            'user_id' => $this->integer()->notNull(),
+            'identity' => $this->string()->notNull(),
+            'network' => $this->string(16)->notNull()
+        ], $tableOptions);
+
+        $this->createIndex('{{%idx-user_networks-identity-name}}', '{{%user_networks}}', ['identity', 'network'], true);
+
+        $this->createIndex('{{%idx-user_networks-user_id}}', '{{%user_networks}}', 'user_id');
+
+        $this->addForeignKey('{{%fk-user_networks-user_id}}', '{{%user_networks}}', 'user_id', '{{%users}}', 'id', 'CASCADE');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $this->dropTable('{{%user_networks}}');
+    }
+}
---
+++ console/migrations/m170808_081301_change_users_field_requirements.php
@@ -0,0 +1,22 @@
+<?php
+
+use yii\db\Migration;
+
+class m170808_081301_change_users_field_requirements extends Migration
+{
+    public function up()
+    {
+        $this->alterColumn('{{%users}}', 'username', $this->string());
+        $this->alterColumn('{{%users}}', 'password_hash', $this->string());
+        $this->alterColumn('{{%users}}', 'email', $this->string());
+
+    }
+
+    public function down()
+    {
+        $this->alterColumn('{{%users}}', 'username', $this->string()->notNull());
+        $this->alterColumn('{{%users}}', 'password_hash', $this->string()->notNull());
+        $this->alterColumn('{{%users}}', 'email', $this->string()->notNull());
+
+    }
+}

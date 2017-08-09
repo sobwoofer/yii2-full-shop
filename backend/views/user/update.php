9a3e8fcb6350284e3e0ef45573a3f33c58e10f18
volynets <volynets@papirus.com.ua>
2017-08-09 13:58:21 +0300

    add user edit for backend

--- backend/views/user/update.php
+++ backend/views/user/update.php
@@ -1,19 +1,28 @@
 <?php
 
-use yii\helpers\Html;
-
 /* @var $this yii\web\View */
+/* @var $model shop\forms\manage\User\UserEditForm */
 /* @var $model shop\entities\User\User */
 
-$this->title = 'Update User: {nameAttribute}';
+use yii\bootstrap\ActiveForm;
+use yii\helpers\Html;
+
+$this->title = 'Update User: ' . $user->id;
 $this->params['breadcrumbs'][] = ['label' => 'Users', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = ['label' => $user->id, 'url' => ['view', 'id' => $user->id]];
 $this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="user-update">
 
-    <?= $this->render('_form', [
-        'model' => $model,
-    ]) ?>
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'username')->textInput(['maxLength' => true]) ?>
+    <?= $form->field($model, 'email')->textInput(['maxLength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
 
 </div>

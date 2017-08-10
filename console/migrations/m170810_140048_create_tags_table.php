80b427540f9cd83ec2096822c52904a64a98d6c2
volynets <volynets@papirus.com.ua>
2017-08-10 17:07:04 +0300

    add table shop_tags migration

---
+++ console/migrations/m170810_140048_create_tags_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `shop_tags`.
+ */
+class m170810_140048_create_tags_table extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
+
+        $this->createTable('{{%shop_tags}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string()->notNull(),
+            'slug' => $this->string()->notNull()
+        ], $tableOptions);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $this->dropTable('{{%shop_tags}}');
+    }
+}

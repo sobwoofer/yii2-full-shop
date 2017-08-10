c6f7a5c474c69cfe42b1b18abf7fbbf1eabdda04
volynets <volynets@papirus.com.ua>
2017-08-10 17:02:18 +0300

    add delete user action

--- backend/controllers/UserController.php
+++ backend/controllers/UserController.php
@@ -126,8 +126,7 @@ class UserController extends Controller
      */
     public function actionDelete($id)
     {
-        $this->findModel($id)->delete();
-
+        $this->service->remove($id);
         return $this->redirect(['index']);
     }
 

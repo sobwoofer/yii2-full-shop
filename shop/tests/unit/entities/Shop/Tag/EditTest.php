bce4e02a56a28bb70035946ca768ca0e49f1fc4b
volynets <volynets@papirus.com.ua>
2017-08-10 17:02:48 +0300

    add tests to tag entity

---
+++ shop/tests/unit/entities/Shop/Tag/EditTest.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: volynets
+ * Date: 10.08.17
+ * Time: 16:51
+ */
+
+namespace tests\unit\entities\Shop\Tag;
+
+
+use Codeception\Test\Unit;
+
+class EditTest extends Unit
+{
+    public function testSuccess()
+    {
+        $tag = Tag::create(
+            $name = 'Name',
+            $slug = 'slug'
+        );
+
+        $tag->edit($name = 'new Name', $slug = 'new-slug');
+
+        $this->assertEquals($name, $tag->name);
+        $this->assertEquals($slug, $tag->slug);
+    }
+}

631e307adcbfd4874ae88be068465a342a0a288c
volynets <volynets@papirus.com.ua>
2017-08-15 12:45:37 +0300

    added shop_categories table migration

---
+++ console/migrations/m170815_092748_create_shop_categories_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `shop_categories`.
+ */
+class m170815_092748_create_shop_categories_table extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $tableOptoons = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=Innodb ';
+
+        $this->createTable('{{%shop_categories}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string()->notNull(),
+            'slug' => $this->string()->notNull(),
+            'title' => $this->string(),
+            'description' => $this->text(),
+            'meta_json' => 'JSON NOT NULL',
+            'lft' => $this->integer()->notNull(),
+            'rgt' => $this->integer()->notNull(),
+            'depth' => $this->integer()->notNull(),
+        ], $tableOptoons);
+
+        $this->createIndex('{{%idx-shop_categories_slug-slug}}', '{{%shop_categories}}', 'slug', true);
+
+        $this->insert('{{%shop_categories}}',[
+            'id' => 1,
+            'name' => '',
+            'slug' => 'root',
+            'title' => null,
+            'description' => null,
+            'meta_json' => '{}',
+            'lft' => 1,
+            'rgt' => 2,
+            'depth' => 0,
+        ]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $this->dropTable('{{%shop_categories}}');
+    }
+}

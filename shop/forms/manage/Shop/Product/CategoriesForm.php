fd7f3961ff1af81a147118046e436d0f129b1a5d
volynets <volynets@papirus.com.ua>
2017-08-15 18:05:21 +0300

    product forms

---
+++ shop/forms/manage/Shop/Product/CategoriesForm.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: volynets
+ * Date: 15.08.17
+ * Time: 17:44
+ */
+namespace shop\forms\manage\Shop\Product;
+
+use yii\base\Model;
+use shop\entities\Shop\Product\Product;
+use yii\helpers\ArrayHelper;
+
+class CategoriesForm extends Model
+{
+    public $main;
+    public $others = [];
+
+    public function __construct(Product $product = null, $config = [])
+    {
+        if ($product) {
+            $this->main = $product->category_id;
+            $this->others = ArrayHelper::getColumn($product->categoryAssignments, 'category_id');
+        }
+        parent::__construct($config);
+    }
+
+    public function rules(): array
+    {
+        return [
+            ['main', 'required'],
+            ['main', 'integer'],
+            ['others', 'each', 'rule' => ['integer']],
+            ['others', 'default', 'value' => []],
+        ];
+    }
+
+}

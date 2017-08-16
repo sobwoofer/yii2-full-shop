5527f6960e0a18a2c468c1b05759082a33d46b14
volynets <volynets@papirus.com.ua>
2017-08-16 15:53:02 +0300

    added shop_values table migration

---
+++ console/migrations/m170816_123422_create_shop_values_table.php
@@ -0,0 +1,56 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `shop_values`.
+ */
+class m170816_123422_create_shop_values_table extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
+
+        $this->createTable('{{%shop_values}}', [
+            'product_id' => $this->integer()->notNull(),
+            'characteristic_id' => $this->integer()->notNull(),
+            'value' => $this->string()
+        ], $tableOptions);
+
+        $this->addPrimaryKey('{{%pk-shop_values}}', '{{%shop_values}}', ['product_id', 'characteristic_id']);
+
+        $this->createIndex('{{%idx-shop_values-product_id}}', '{{%shop_values}}', 'product_id');
+        $this->createIndex('{{%idx-shop_values-characteristic_id}}', '{{%shop_values}}', 'characteristic_id');
+
+        $this->addForeignKey(
+            '{{%fk-shop_values-product_id}}',
+            '{{%shop_values}}',
+            'product_id',
+            '{{%shop_products}}',
+            'id',
+            'CASCADE',
+            'RESTRICT'
+        );
+
+        $this->addForeignKey(
+            '{{%idx-shop_value-characteristic_id}}',
+            '{{%shop_values}}',
+            'characteristic_id',
+            '{{%shop_characteristics}}',
+            'id',
+            'CASCADE',
+            'RESTRICT'
+        );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $this->dropTable('{{%shop_values}}');
+    }
+}

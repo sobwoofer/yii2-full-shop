5a3d037add8e413256ea434cdac84ab33bc01700
volynets <volynets@papirus.com.ua>
2017-09-04 13:16:28 +0300

    add brand view

---
+++ frontend/views/shop/catalog/brand.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: volynets
+ * Date: 04.09.17
+ * Time: 13:14
+ */
+
+
+/* @var $this yii\web\View */
+/* @var $dataProvider yii\data\DataProviderInterface */
+/* @var $brand core\entities\Shop\Brand */
+
+use yii\helpers\Html;
+
+$this->title = $brand->getSeoTitle();
+
+$this->registerMetaTag(['name' =>'description', 'content' => $brand->meta->description]);
+$this->registerMetaTag(['name' =>'keywords', 'content' => $brand->meta->keywords]);
+
+$this->params['breadcrumbs'][] = ['label' => 'Catalog', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $brand->name;
+?>
+
+<h1><?= Html::encode($brand->name) ?></h1>
+
+<hr />
+
+<?= $this->render('_list', [
+    'dataProvider' => $dataProvider
+]) ?>
+
+

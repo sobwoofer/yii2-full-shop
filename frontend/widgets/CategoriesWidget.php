495f2e5cacd1984a8092ec9aac06f4bd18785659
volynets <volynets@papirus.com.ua>
2017-09-07 14:48:34 +0300

    added category aggregates

--- frontend/widgets/CategoriesWidget.php
+++ frontend/widgets/CategoriesWidget.php
@@ -12,27 +12,49 @@ use core\entities\Shop\Category;
 use core\readModels\Shop\CategoryReadRepository;
 use yii\base\Widget;
 use yii\helpers\Html;
+use Elasticsearch\Client;
+use yii\helpers\ArrayHelper;
 
 class CategoriesWidget extends Widget
 {
     /** @var Category|null */
     public $active;
+    private $client;
 
     private $categories;
 
-    public function __construct(CategoryReadRepository $categories, $config = [])
+    public function __construct(CategoryReadRepository $categories, Client $client,  $config = [])
     {
         parent::__construct($config);
         $this->categories = $categories;
+        $this->client = $client;
     }
 
     public function run(): string
     {
-        return Html::tag('div', implode(PHP_EOL, array_map(function (Category $category) {
+        $aggs = $this->client->search([
+            'index' => 'shop',
+            'type' => 'products',
+            'body' => [
+                'size' => 0,
+                'aggs' => [
+                    'group_by_category' => [
+                        'terms' => [
+                            'field' => 'categories'
+                        ]
+                    ]
+                ],
+            ],
+        ]);
+
+        $counts = ArrayHelper::map($aggs['aggregations']['group_by_category']['buckets'], 'key', 'doc_count');
+
+        return Html::tag('div', implode(PHP_EOL, array_map(function (Category $category) use ($counts) {
             $indent = ($category->depth > 1 ? str_repeat('&nbsp;&nbsp;&nbsp;', $category->depth - 1) . '- ' : '');
             $active = $this->active && ($this->active->id == $category->id || $this->active->isChildOf($category));
+            $count = ArrayHelper::getValue($counts, $category->id, 0);
             return Html::a(
-                $indent . Html::encode($category->name),
+                $indent . Html::encode($category->name) . ' (' . $count . ')',
                 ['/shop/catalog/category', 'id' => $category->id],
                 ['class' => $active ? 'list-group-item active' : 'list-group-item']
             );

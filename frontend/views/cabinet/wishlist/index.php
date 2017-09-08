c1ff103d506232847ce065cf0be2df6de319cb67
volynets <volynets@papirus.com.ua>
2017-09-08 10:52:04 +0300

    fixwishlist view

--- frontend/views/cabinet/wishlist/index.php
+++ frontend/views/cabinet/wishlist/index.php
@@ -111,39 +111,3 @@ $this->params['breadcrumbs'][] = $this->title;
 </div>
 
 
-
-<div class="cabinet-index">
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'columns' => [
-            [
-                'value' => function (Product $model) {
-                    return $model->mainPhoto ? Html::img($model->mainPhoto->getThumbFileUrl('file', 'admin')) : null;
-                },
-                'format' => 'raw',
-                'contentOptions' => ['style' => 'width: 100px'],
-            ],
-            'id',
-            [
-                'attribute' => 'name',
-                'value' => function (Product $model) {
-                    return Html::a(Html::encode($model->name), ['/shop/catalog/product', 'id' => $model->id]);
-                },
-                'format' => 'raw',
-            ],
-            [
-                'attribute' => 'price_new',
-                'value' => function (Product $model) {
-                    return PriceHelper::format($model->price_new);
-                },
-            ],
-            [
-                'class' => ActionColumn::class,
-                'template' => '{delete}',
-            ],
-        ],
-    ]); ?>
-
-</div>

37eddff3d22f48a4c0b440eae651c1ca17b2b90e
volynets <volynets@papirus.com.ua>
2017-09-08 15:58:32 +0300

    added TODO db storage for cart

---
+++ core/cart/storage/DbStorage.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: volynets
+ * Date: 08.09.17
+ * Time: 15:55
+ */
+
+namespace core\cart\storage;
+
+
+class DbStorage implements StorageInterface
+{
+    public function load(): array
+    {
+        // TODO: Implement load() method.
+    }
+
+    public function save(array $items): void
+    {
+        // TODO: Implement save() method.
+    }
+
+}

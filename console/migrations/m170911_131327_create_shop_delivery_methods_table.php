57484ba9ad62c7bf63afd679bd8593cef949eae6
volynets <volynets@papirus.com.ua>
2017-09-11 16:36:29 +0300

    added delivery table

---
+++ console/migrations/m170911_131327_create_shop_delivery_methods_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `shop_delivery_methods`.
+ */
+class m170911_131327_create_shop_delivery_methods_table extends Migration
+{
+    public function up()
+    {
+        $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
+
+        $this->createTable('{{%shop_delivery_methods}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string()->notNull(),
+            'cost' => $this->integer()->notNull(),
+            'min_weight' => $this->integer(),
+            'max_weight' => $this->integer(),
+            'sort' => $this->integer()->notNull(),
+        ], $tableOptions);
+    }
+
+    public function down()
+    {
+        $this->dropTable('{{%shop_delivery_methods}}');
+    }
+}

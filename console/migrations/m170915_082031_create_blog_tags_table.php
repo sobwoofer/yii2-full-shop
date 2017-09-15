07d489a41b880e9bdbf5a91f0422181dbd37166b
volynets <volynets@papirus.com.ua>
2017-09-15 12:02:18 +0300

    added blog tags table

---
+++ console/migrations/m170915_082031_create_blog_tags_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `blog_tags`.
+ */
+class m170915_082031_create_blog_tags_table extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+
+        $this->createTable('{{%blog_tags}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string()->notNull(),
+            'slug' => $this->string()->notNull()
+        ], $tableOptions);
+
+        $this->createIndex('{{idx_blog_tags-slug}}','{{%blog_tags}}','slug', true);
+
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $this->dropTable('{{%blog_tags}}');
+    }
+}

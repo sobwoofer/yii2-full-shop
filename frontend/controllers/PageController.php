3aabc90d00eb68f4bc57afe3de180acffb372247
volynets <volynets@papirus.com.ua>
2017-09-21 10:46:58 +0300

    added pages to frontend

---
+++ frontend/controllers/PageController.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: volynets
+ * Date: 21.09.17
+ * Time: 10:44
+ */
+
+
+namespace frontend\controllers;
+
+use core\readModels\PageReadRepository;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+
+/**
+ * PagesController implements the CRUD actions for Page model.
+ */
+class PageController extends Controller
+{
+    private $pages;
+
+    public function __construct($id, $module, PageReadRepository $pages, $config = [])
+    {
+        parent::__construct($id, $module, $config);
+        $this->pages = $pages;
+    }
+
+    /**
+     * @param $id
+     * @return mixed
+     * @throws NotFoundHttpException
+     * @internal param string $slug
+     */
+    public function actionView($id)
+    {
+        if (!$page = $this->pages->find($id)) {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+
+        return $this->render('view', [
+            'page' => $page,
+        ]);
+    }
+}

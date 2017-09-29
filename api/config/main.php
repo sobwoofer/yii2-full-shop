78be9bb22617a599d4dc08ee7e82bc7e2160c608
volynets <volynets@papirus.com.ua>
2017-09-29 16:59:37 +0300

    added OAuth module

--- api/config/main.php
+++ api/config/main.php
@@ -32,7 +32,25 @@ return [
             ],
         ],
     ],
-    'modules' => [],
+    'modules' => [
+        'oauth2' => [
+            'class' => 'filsh\yii2\oauth2server\Module',
+            'tokenParamName' => 'accessToken',
+            'tokenAccessLifetime' => 3600 * 24,
+            'storageMap' => [
+                'user_credentials' => 'common\auth\Identity',
+            ],
+            'grantTypes' => [
+                'user_credentials' => [
+                    'class' => 'OAuth2\GrantType\UserCredentials',
+                ],
+                'refresh_token' => [
+                    'class' => 'OAuth2\GrantType\RefreshToken',
+                    'always_issue_new_refresh_token' => true
+                ]
+            ]
+        ]
+    ],
     'components' => [
         'request' => [
             'parsers' => [
--- common/auth/Identity.php
+++ common/auth/Identity.php
@@ -8,21 +8,25 @@
 
 namespace common\auth;
 
+use filsh\yii2\oauth2server\Module;
+use OAuth2\Storage\UserCredentialsInterface;
 use core\entities\User\User;
 use core\readModels\UserReadRepository;
 use yii\base\NotSupportedException;
 use yii\web\IdentityInterface;
+use Yii;
 
 /**
  * Этот клас создан для переноса всех необходимых методов из сущьности User которые производят логин и идентивикацию пользователей
  * а также реализуют интерфейс IdentityInterface. теперь у всех конфигурационных файлах он должен быть указан как
  * identityClass, так же сюда вставлены методы из дополнения OAuth2 для авторизации через API и генерации всех необходимых
- * токенов и ключей.
+ * токенов и ключей. При создании обьекта служит оберткой класса User и возврящает по запросу данные непосредственно
+ * из обьекта класса User.
  *
  * Class Identity
  * @package common\auth
  */
-class Identity implements IdentityInterface
+class Identity implements IdentityInterface, UserCredentialsInterface
 {
     private $user;
 
@@ -39,7 +43,8 @@ class Identity implements IdentityInterface
 
     public static function findIdentityByAccessToken($token, $type = null)
     {
-        throw new NotSupportedException('"findIdentityByAccessToken" is not implemented.');
+        $data = self::getOauth()->getServer()->getResourceController()->getToken();
+        return !empty($data['user_id']) ? static::findIdentity($data['user_id']) : null;
     }
 
     public function getId(): int
@@ -57,8 +62,27 @@ class Identity implements IdentityInterface
         return $this->getAuthKey() === $authKey;
     }
 
+    public function checkUserCredentials($username, $password): bool
+    {
+        if (!$user = self::getRepository()->findActiveByUsername($username)) {
+            return false;
+        }
+        return $user->validatePassword($password);
+    }
+
+    public function getUserDetails($username): array
+    {
+        $user = self::getRepository()->findActiveByUsername($username);
+        return ['user_id' => $user->id];
+    }
+
     private static function getRepository(): UserReadRepository
     {
         return \Yii::$container->get(UserReadRepository::class);
+    }
+
+    private static function getOauth(): Module
+    {
+        return Yii::$app->getModule('oauth2');
     }
 }

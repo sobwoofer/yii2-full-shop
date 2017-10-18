070937fb92091428c1c6ae697c58e17e138db9bc
volynets <volynets@papirus.com.ua>
2017-10-18 17:21:51 +0300

    added some instructions

--- common/bootstrap/SetUp.php
+++ common/bootstrap/SetUp.php
@@ -36,28 +36,35 @@ class SetUp implements BootstrapInterface
 {
     public function bootstrap($app): void
     {
+        //фреймворковский контейнер, дальше внедряем в него зависимости
         $container = \Yii::$container;
 
+        //Подключение билдера Еластик Серча для генерации и импорта JSON(а)
         $container->setSingleton(Client::class, function () {
             return ClientBuilder::create()->build();
         });
 
+        //Подключение фреймворковского мейлера из приложения $app
         $container->setSingleton(MailerInterface::class, function () use ($app) {
             return $app->mailer;
         });
 
+        //Подключение компонента кеширования
         $container->setSingleton(Cache::class, function () use ($app) {
             return $app->cache;
         });
 
+        //Подключение менеджера RBAC ролей пользователей
         $container->setSingleton(ManagerInterface::class, function () use ($app) {
             return $app->authManager;
         });
 
+        //Подключение Сервиса обратной связи и передача ему почты из конфига
         $container->setSingleton(ContactService::class, [], [
             $app->params['adminEmail']
         ]);
 
+        //Подключение корзины и указание параметров куда писать пользовательскую корзину
         $container->setSingleton(Cart::class, function () use ($app) {
             return new Cart(
                 new HybridStorage($app->get('user'), 'cart', 3600 * 24, $app->db),
@@ -65,10 +72,12 @@ class SetUp implements BootstrapInterface
             );
         });
 
+        //Подключение генерации xml фида данных для сервисов типа хотлайн и тп.
         $container->setSingleton(Market::class, [], [
             new ShopInfo($app->name, $app->name, $app->params['frontendHostInfo']),
         ]);
 
+        //Подключение сервиса почтовой подписки на рассылку
         $container->setSingleton(Newsletter::class, function () use ($app) {
             return new MailChimp(
                 new \DrewM\MailChimp\MailChimp($app->params['mailChimpKey']),
@@ -76,6 +85,7 @@ class SetUp implements BootstrapInterface
             );
         });
 
+        //Подключение смс рассылщика
         $container->setSingleton(SmsSender::class, function () use ($app) {
             return new LoggedSender(
                 new SmsRu($app->params['smsRuKey']),
@@ -83,6 +93,7 @@ class SetUp implements BootstrapInterface
             );
         });
 
+        //Подключение обработчика событий
         $container->setSingleton(EventDispatcher::class, function (Container $container) {
             return new SimpleEventDispatcher([
                 UserSignUpRequested::class => [

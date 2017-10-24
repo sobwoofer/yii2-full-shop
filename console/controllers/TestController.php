cf35db81f0b30576e1d11dabb4666d1f7ba567eb
volynets <volynets@papirus.com.ua>
2017-10-24 16:31:15 +0300

    added test queue console controller

---
+++ console/controllers/TestController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: volynets
+ * Date: 24.10.17
+ * Time: 14:40
+ */
+
+namespace console\controllers;
+
+use yii\console\Controller;
+use Yii;
+
+class TestController extends Controller
+{
+    public function actionAddQueue()
+    {
+        Yii::$app->queue->push(new TestQueueJob([
+            'name' => 'vasya'
+        ]));
+    }
+
+
+}
---
+++ console/controllers/TestQueueJob.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: volynets
+ * Date: 24.10.17
+ * Time: 14:42
+ */
+
+namespace console\controllers;
+
+
+use yii\base\Object;
+use yii\queue\Job;
+
+class TestQueueJob extends Object implements Job
+{
+    public $name;
+
+    public function execute($queue)
+    {
+        file_put_contents(__DIR__ . '/1.txt', $this->name);
+    }
+
+}

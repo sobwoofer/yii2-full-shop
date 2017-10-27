329b770fcade61f53ac45228e3f9450cc2dc67f9
volynets <volynets@papirus.com.ua>
2017-10-27 16:06:40 +0300

    added pay-deliver page to view

---
+++ frontend/views/contact/pay-deliver.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: volynets
+ * Date: 27.10.17
+ * Time: 16:02
+ */
+
+?>
+
+<div class="pay_deliver-page">
+    <div class="container">
+        <h1 class="main-title">Оплата и доставка</h1>
+        <div class="row">
+            <div class="col-md-6">
+                <div>
+                    <p class="text-center"><b>Оплата</b></p>
+                    <!-- Nav tabs -->
+                    <p class="text-center">Для удобства клиентов мы предлагаем 3 варианта оплаты</p>
+                    <br>
+                    <ul class="nav nav-tabs" role="tablist">
+                        <li role="presentation" class="active"><a href="#tab-1" aria-controls="tab-1" role="tab" data-toggle="tab">Наличный расчет
+                            </a></li>
+                        <li role="presentation"><a href="#tab-2" aria-controls="tab-2" role="tab" data-toggle="tab">Оплата онлайн,
+                                картой</a></li>
+                        <li role="presentation"><a href="#tab-3" aria-controls="tab-3" role="tab" data-toggle="tab">Безналичный расчет</a></li>
+                    </ul>
+
+                    <!-- Tab panes -->
+                    <div class="tab-content">
+                        <div role="tabpanel" class="tab-pane active" id="tab-1">Вы оплачиваете заказ наличными курьеру в момент
+                            получения заказа.
+                        </div>
+                        <div role="tabpanel" class="tab-pane" id="tab-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet asperiores, aspernatur cumque delectus deserunt eius, eligendi esse, fuga illo ipsam maiores minima odit quaerat quidem sint ut voluptates. Atque, nesciunt.</div>
+                        <div role="tabpanel" class="tab-pane" id="tab-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias autem, dignissimos distinctio eaque facilis harum impedit odio optio soluta. Aperiam illo nobis quam quod reprehenderit saepe, unde voluptate? Non.</div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="hidden visible-xs visible-sm">
+                <br>
+                <br>
+            </div>
+
+            <div class="col-md-6">
+                <div>
+                    <p class="text-center"><b>Доставка</b></p>
+                    <!-- Nav tabs -->
+                    <p> &nbsp; </p>
+                    <br>
+                    <ul class="nav nav-tabs" role="tablist">
+                        <li role="presentation" class="active"><a href="#tab-4" aria-controls="tab-4" role="tab" data-toggle="tab">Киев
+                            </a></li>
+                        <li role="presentation"><a href="#tab-5" aria-controls="tab-5" role="tab" data-toggle="tab">Днепр</a></li>
+                        <li role="presentation"><a href="#tab-6" aria-controls="tab-6" role="tab" data-toggle="tab">Киевская
+                                область</a></li>
+                        <li role="presentation"><a href="#tab-7" aria-controls="tab-7" role="tab" data-toggle="tab">Украина</a></li>
+                    </ul>
+
+                    <!-- Tab panes -->
+                    <div class="tab-content">
+                        <div role="tabpanel" class="tab-pane active" id="tab-4">tab-4
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam eaque eligendi enim ex explicabo fugit itaque maiores maxime modi mollitia nemo neque nesciunt nulla provident, sint sit suscipit voluptas voluptates.
+                            <br>
+                            <br>
+                            <br>
+                            <br>
+                            <br>
+                        </div>
+                        <div role="tabpanel" class="tab-pane" id="tab-5">tab-5</div>
+                        <div role="tabpanel" class="tab-pane" id="tab-6">
+                            <p>При заказе на сумму от 500-75t0 грн.  - стоимость доставки 35 грн</p>
+                            <p>При заказе на сумму 750 грн. и более - доставка бесплатная</p>
+                            <br>
+                            <br>
+                            <p>Доставка осуществляется в рабочие дни с 9:00 до 18:00. При размещении заказа до 15:00 доставка
+                                выполняется на следующий рабочий день, при размещении заказа после 15:00 доставка выполняется через один рабочий день.</p>
+                            <br>
+                            <p>Внимание! При безналичной оплате - доставка в течении  3 рабочих дней после поступления денег.</p>
+                        </div>
+                        <div role="tabpanel" class="tab-pane" id="tab-7">tab-7</div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <!--<div class="row">-->
+        <div class="map">
+
+            <p class="map-text" style="margin: 30px 0;">
+                Также вы можете оплатить заказ в нашем офисе и забрать его самостоятельно: г. Вишневое, ул. Киевская, 2 А</p>
+
+
+            <div class="google-maps">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d20325.69671712946!2d30.505818324172036!3d50.44646322349638!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4ce5791674ff5%3A0xf04c07850e2acf98!2z0JfQvtC70L7RgtGL0LUg0LLQvtGA0L7RgtCw!5e0!3m2!1sru!2sru!4v1497902473996" frameborder="0" style="border:0" allowfullscreen></iframe>
+            </div>
+        </div>
+        <!--</div>-->
+    </div>
+</div>

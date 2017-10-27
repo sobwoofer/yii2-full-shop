a1d1d50f6fa77f622b382e3c6342b0608067ebcd
volynets <volynets@papirus.com.ua>
2017-10-27 16:06:21 +0300

    added faq page to view

---
+++ frontend/views/blog/faq.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: volynets
+ * Date: 27.10.17
+ * Time: 15:46
+ */
+
+?>
+
+
+<div class="container">
+    <h1 class="main-title">ВОПРОСЫ И ОТВЕТЫ</h1>
+    <div>
+
+        <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+            <div class="panel_custom " role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                <div class="panel_custom__heading" role="tab" id="headingOne">
+                    <h4 class="panel_custom__title">
+                        <a class="collapsed">
+                            <i>Как я могу зарегестрироваться?</i>
+                        </a>
+                        <span class="caret"></span>
+                    </h4>
+                </div>
+                <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+                    <div class="panel-body">
+                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat
+                        skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid
+                        single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea
+                        proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably
+                        haven't heard of them accusamus labore sustainable VHS.
+                    </div>
+                </div>
+            </div>
+            <div class="panel_custom collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false"
+                 aria-controls="collapseTwo">
+                <div class="panel_custom__heading" role="tab" id="headingTwo">
+                    <h4 class="panel_custom__title">
+                        <a class="collapsed">
+                            <i>Как выбрать сервисную компанию?</i>
+                        </a>
+                        <span class="caret"></span>
+                    </h4>
+                </div>
+                <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+                    <div class="panel-body">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos optio vero voluptas! Aliquam dolorem doloremque esse excepturi hic minima modi
+                        necessitatibus numquam praesentium quisquam, reprehenderit, suscipit tenetur unde ut voluptates.
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos optio vero voluptas! Aliquam dolorem doloremque esse excepturi hic minima modi
+                        necessitatibus numquam praesentium quisquam, reprehenderit, suscipit tenetur unde ut voluptates.
+                    </div>
+                </div>
+            </div>
+            <div class="panel_custom collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false"
+                 aria-controls="collapseThree">
+                <div class="panel_custom__heading" role="tab" id="headingThree">
+                    <h4 class="panel_custom__title">
+                        <a class="collapsed">
+                            <i>Какие есть способы доставки товара?</i>
+                        </a>
+                        <span class="caret"></span>
+                    </h4>
+                </div>
+                <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+                    <div class="panel-body">
+                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat
+                        skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid
+                        single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea
+                        proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably
+                        haven't heard of them accusamus labore sustainable VHS.
+                    </div>
+                </div>
+            </div>
+            <div class="panel_custom collapsed" role="button" data-toggle="collapse" data-parent="#accordion"
+                 href="#collapseFore" aria-expanded="false" aria-controls="collapseFore">
+                <div class="panel_custom__heading" role="tab" id="headingFore">
+                    <h4 class="panel_custom__title">
+                        <a class="collapsed">
+                            <i>Как можно оплатить товар?</i>
+                        </a>
+                        <span class="caret"></span>
+                    </h4>
+                </div>
+                <div id="collapseFore" class="panel-collapse collapse" role="tabpanel"
+                     aria-labelledby="headingFore">
+                    <div class="panel-body">
+                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat
+                        skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid
+                        single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea
+                        proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably
+                        haven't heard of them accusamus labore sustainable VHS.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>

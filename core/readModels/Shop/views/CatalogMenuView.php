b94d7cc4e0979ffcaadb822b386513ea57cf6e1a
volynets <volynets@papirus.com.ua>
2017-11-01 11:53:55 +0200

    added catalog menu object

---
+++ core/readModels/Shop/views/CatalogMenuView.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: volynets
+ * Date: 31.10.17
+ * Time: 15:47
+ */
+
+namespace core\readModels\Shop\views;
+
+
+use core\entities\Shop\Category;
+
+class CatalogMenuView
+{
+    public $category;
+    public $_children;
+
+    public function __construct(Category $category)
+    {
+        $this->category = $category;
+
+    }
+
+    public function addChild(Category $child)
+    {
+        return $this->_children[] = new self($child);
+
+//        return static();
+    }
+
+}

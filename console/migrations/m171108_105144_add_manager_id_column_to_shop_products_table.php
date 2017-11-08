e59c05f3b8bdc00fe9f9c77c4e74a8058de355f1
volynets <volynets@papirus.com.ua>
2017-11-08 14:14:06 +0200

    added manager id column to product table

---
+++ console/migrations/m171108_105144_add_manager_id_column_to_shop_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m171108_105144_add_columns_to_shop_products_table
+ */
+class m171108_105144_add_manager_id_column_to_shop_products_table extends Migration
+{
+
+
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+        $this->addColumn('{{%shop_products}}', 'manager_id', $this->integer());
+        $this->addForeignKey(
+            '{{%fk-shop_products-manager_id}}',
+            '{{%shop_products}}',
+            'manager_id',
+            '{{%users}}',
+            'id',
+            'CASCADE',
+            'CASCADE'
+        );
+
+    }
+
+    public function down()
+    {
+        echo "m171108_105144_add_columns_to_shop_products_table cannot be reverted.\n";
+
+        return false;
+    }
+
+}
